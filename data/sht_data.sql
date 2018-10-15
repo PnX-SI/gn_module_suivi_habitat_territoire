3eba56ba3ecb659844f72c4476e65f558b7b672d
cdcvidal <celine_vidal@natural-solutions.eu>
2018-10-15 14:38:23 +0200

    add data script

---
+++ data/sht_data.sql
@@ -0,0 +1,53 @@
+-- parametrer ref_geo.bib_areas_types -- 
+-- créer le type de mailles 100*100 
+-- problème : ne supporte pas dimension Z => suppression info dans QGIS
+INSERT INTO ref_geo.bib_areas_types (type_name, type_code, type_desc)
+VALUES ('Mailles100*100m', 'M100m', 'Maille INPN redécoupé en 100m');
+
+--insérer les mailles dans l_areas grâce au fichier maille_tmp
+INSERT INTO ref_geo.l_areas (id_type, area_name, area_code, geom, centroid, source)
+SELECT ref_geo.get_id_area_type('M100m'), name, name, geom, ST_CENTROID(geom), 'INPN'
+FROM pr_monitoring_habitat_territory.maille_tmp; 
+
+-- insérer les mailles dans li_grids
+INSERT INTO ref_geo.li_grids
+SELECT area_code, id_area, ST_XMin(ST_Extent(geom)), ST_XMax(ST_Extent(geom)), ST_YMin(ST_Extent(geom)),ST_YMax(ST_Extent(geom))
+FROM ref_geo.l_areas
+WHERE id_type=ref_geo.get_id_area_type('M100m')
+GROUP by area_code, id_area;
+
+
+-- créer nomenclature  HAB --  
+INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_default, label_fr, definition_fr, source )
+VALUES (ref_nomenclatures.get_id_nomenclature_type('TYPE_SITE'), 'HAB', 'Zone de habitat', 'Zone de habitat - suivi habitat territoire', 'Zone d''habitat',  'Zone d''habitat issu du module suivi habitat territoire', 'CBNA');
+
+
+-- TODO: insérer les données dans t_base_sites grâce à celles dans la table zb_tmp
+
+
+
+-- insérer les données dans t_base_sites grâce à celles dans la table maille_tmp
+-- ATTENTION: il faut que le zp_tmp.shp soit en 2154, sinon ça donne des erreurs pour afficher les Zp.  
+/*INSERT INTO gn_monitoring.t_base_sites
+(id_nomenclature_type_site, base_site_name, base_site_description,  base_site_code, first_use_date, geom )
+SELECT ref_nomenclatures.get_id_nomenclature('TYPE_SITE', 'HAB'), 'HAB-', '', name, now(), ST_TRANSFORM(ST_SetSRID(geom, MY_SRID_LOCAL), MY_SRID_WORLD)
+FROM pr_monitoring_flora_territory.maille_tmp;*/
+INSERT INTO gn_monitoring.t_base_sites
+(id_nomenclature_type_site, base_site_name, base_site_description,  base_site_code, first_use_date, geom )
+SELECT ref_nomenclatures.get_id_nomenclature('TYPE_SITE', 'HAB'), 'HAB-', '', name, now(), ST_TRANSFORM(ST_SetSRID(geom, MY_SRID_LOCAL), MY_SRID_WORLD)
+FROM pr_monitoring_flora_territory.maille_tmp;
+
+--- update le nom du site pour y ajouter l'identifiant du site
+UPDATE gn_monitoring.t_base_sites SET base_site_name=CONCAT (base_site_name, id_base_site); 
+
+-- extension de la table t_base_sites : mettre les données dans t_infos_site
+-- TEST --
+INSERT INTO pr_monitoring_habitat_territory.t_infos_site (id_base_site, cd_hab)
+VALUES (17,16265);
+-- TEST --
+/*
+INSERT INTO pr_monitoring_habitat_territory.t_infos_site (id_base_site, cd_hab)
+SELECT id_base_site, zh.cd_hab
+FROM gn_monitoring.t_base_sites bs
+JOIN pr_monitoring_habitat_territory.zh_tmp zp ON zh.id::character varying = bs.base_site_code;
+*/
--- install_db.sh
+++ install_db.sh
@@ -15,6 +15,26 @@ fi
 
 # Copy SQL files into /tmp system folder in order to edit it with variables
 cp data/habitat.sql /tmp/habitat.sql
+cp data/sht.sql /tmp/sht.sql
 
-# Create SFT schema into GeoNature database
+cp data/sht_perturbations.sql /tmp/sht_perturbations.sql
+cp data/habitat_data.sql /tmp/habitat_data.sql
+cp data/sht_data.sql /tmp/sht_data.sql
+
+
+# Create habitat schema into GeoNature database
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/habitat.sql &>> var/log/install_habitat.log
+
+# Create SHT schema into GeoNature database
+export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/sht.sql &>> var/log/install_sht.log
+
+
+# Include sample data into database
+if $insert_sample_data
+then
+    #sudo -n -u postgres -s shp2pgsql -W "UTF-8" -s 2154 -D -I /tmp/mailles100z.shp pr_monitoring_habitat_territory.maille_tmp | psql -h localhost -U geonatuser -d geonaturedb &>> var/log/install_sft.log
+    sudo -n -u postgres -s shp2pgsql -W "UTF-8" -s 2154 -D -I /tmp/Mailles100.shp pr_monitoring_habitat_territory.maille_tmp | psql -h $db_host -U $user_pg -d $db_name &>> var/log/install_sht.log
+    export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/habitat_data.sql &>> var/log/install_habitat_data.log
+    export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/sht_perturbations.sql &>> var/log/install_sht_perturbations.log
+    export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/sht_data.sql &>>  var/log/install_sht_data.log
+fi
