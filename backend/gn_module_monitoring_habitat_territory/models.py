1c4ab4ddae023ae7968b95286ff1b6c02c9ba834
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2025-09-16 16:14:53 +0200

    fix(backend): check permssion with organism without error

--- backend/gn_module_monitoring_habitat_territory/models.py
+++ backend/gn_module_monitoring_habitat_territory/models.py
@@ -63,7 +63,7 @@ class VisitAuthMixin(object):
 
     def user_is_in_organism_of_visit(self, user):
         for obs in self.observers:
-            if obs.id_organisme == user.id_organism:
+            if obs.id_organisme == user.id_organisme:
                 return True
         return False
     
